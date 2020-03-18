Project-Xtended/vendor_xtended

Themes: Add DU themes [2/3]

config/common.mk
@@ -287,6 +287,9 @@ DEVICE_PACKAGE_OVERLAYS += vendor/xtended/overlay/common
 -include vendor/xtended/config/version.mk
 -include vendor/xtended/config/partner_gms.mk
 
+# Include Xtended theme files
+include vendor/xtended/themes/backgrounds/themes.mk
+
 # Fonts
 PRODUCT_COPY_FILES += \
     vendor/xtended/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \

ProjectSakura/vendor_sakura

Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

config/common.mk
@@ -260,6 +260,8 @@ SAKURA_VERSION := $(LINEAGE_VERSION)
 # Bootanimation
 include vendor/lineage/config/bootanimation.mk
 
+# Include Sakura's theme files
+include vendor/lineage/themes/backgrounds/themes.mk
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
-