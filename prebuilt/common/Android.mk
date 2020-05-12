3df431d29aad222340443a32659328ec9060a187
LuK1337 <priv.luk@gmail.com>
2020-05-13 00:01:38 +0200

    config: Install {Lato,Rubik} fonts and their overlay packages
    Change-Id: I3fe93982095722d22e726baf9561a6bde31f5bf1
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -73,3 +73,10 @@ LOCAL_DEX_PREOPT := false
 LOCAL_REPLACE_PREBUILT_APK_INSTALLED := $(LOCAL_PATH)/$(LOCAL_SRC_FILES)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := fonts_customization.xml
+LOCAL_SRC_FILES := etc/fonts_customization.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_PRODUCT_MODULE := true
+include $(BUILD_PREBUILT)
