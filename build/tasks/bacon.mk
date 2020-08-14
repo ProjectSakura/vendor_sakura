f044de72044b5078c233e7af55fa36c9e6171e84
Akhil Narang <akhilnarang.1999@gmail.com>
2020-08-14 18:48:36 +0200

    Sakura: generate ota json after build completion
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>
    Change-Id: I28cd436bb61289def1c9ea576772acf6af3e88b2
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -25,3 +25,5 @@ bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(LINEAGE_TARGET_PACKAGE)
 	$(hide) $(SHA256) $(LINEAGE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(LINEAGE_TARGET_PACKAGE).sha256sum
 	@echo "Package Complete: $(LINEAGE_TARGET_PACKAGE)" >&2
+	@echo "Creating json OTA..." >&2
+	$(hide) ./vendor/lineage/build/tools/createjson.sh
