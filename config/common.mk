9d523f68e450606724a40bbb4e008691e338b61c
Pranav Vashi <neobuddy89@gmail.com>
2020-08-24 10:40:08 +0200

    Vendor: notch-city: Add 3 mode display cutout handler [2b/3]
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -78,7 +78,7 @@ PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/org.lineageos.android.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.android.xml \
     vendor/lineage/config/permissions/privapp-permissions-lineage-system.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-lineage.xml \
     vendor/lineage/config/permissions/privapp-permissions-lineage-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-lineage.xml \
-     vendor/lineage/config/permissions/privapp-permissions-sakura-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-sakura.xml \
+    vendor/lineage/config/permissions/privapp-permissions-sakura-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-sakura.xml \
     vendor/lineage/config/permissions/privapp-permissions-cm-legacy.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-cm-legacy.xml
 
 # Pre-granted permissions
@@ -339,6 +339,12 @@ endif
 # Fix Dialer
 PRODUCT_COPY_FILES +=  \
     vendor/lineage/prebuilt/common/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
+
+# Notch city
+PRODUCT_PACKAGES += \
+    HideCutout \
+    StatusBarStock \
+    StatusBarStockSysUI
 
 # Include Sakura's theme files
 include vendor/lineage/themes/backgrounds/themes.mk
