8ee34ebf6542bcf6d334b3377cb39bd74426d485
LordShenron <shen.priyanshu@gmail.com>
2020-08-29 13:03:50 +0200

    Vendor: ota json generator: use different text colors
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- build/tools/createjson.sh
+++ build/tools/createjson.sh
@@ -1,15 +1,23 @@
-echo "=========================================================================="
-echo "* ╭━━━╮╱╱╱╱╱╱╱╱╱╱╱╭╮╱╭━━━╮╱╱╭╮                                            "
-echo "* ┃╭━╮┃╱╱╱╱╭╮╱╱╱╱╭╯╰╮┃╭━╮┃╱╱┃┃                                            "
-echo "* ┃╰━╯┣━┳━━╋╋━━┳━┻╮╭╯┃╰━━┳━━┫┃╭┳╮╭┳━┳━━╮                                  "
-echo "* ┃╭━━┫╭┫╭╮┣┫┃━┫╭━┫┃╱╰━━╮┃╭╮┃╰╯┫┃┃┃╭┫╭╮┃                                  "
-echo "* ┃┃╱╱┃┃┃╰╯┃┃┃━┫╰━┫╰╮┃╰━╯┃╭╮┃╭╮┫╰╯┃┃┃╭╮┃                                  "
-echo "* ╰╯╱╱╰╯╰━━┫┣━━┻━━┻━╯╰━━━┻╯╰┻╯╰┻━━┻╯╰╯╰╯                                  "
-echo "* ╱╱╱╱╱╱╱╱╭╯┃                                                             "
-echo "* ╱╱╱╱╱╱╱╱╰━╯                                                             "
-echo "* THANK YOU FOR BUILDING PROJECT SAKURA                                   "
-echo "=========================================================================="
-echo "You can use the below generated json to push OTA."
+# Specify colors utilized in the terminal
+red='\033[0;31m'             #  red
+grn='\033[0;32m'             #  green
+ylw='\033[0;33m'             #  yellow
+blu='\033[0;34m'             #  blue
+ppl='\033[0;35m'             #  purple
+cya='\033[0;36m'             #  cyan
+
+echo -e ${blu}"=========================================================================="
+echo -e ${ylw}"* ╭━━━╮╱╱╱╱╱╱╱╱╱╱╱╭╮╱╭━━━╮╱╱╭╮                                            "
+echo -e ${ylw}"* ┃╭━╮┃╱╱╱╱╭╮╱╱╱╱╭╯╰╮┃╭━╮┃╱╱┃┃                                            "
+echo -e ${ylw}"* ┃╰━╯┣━┳━━╋╋━━┳━┻╮╭╯┃╰━━┳━━┫┃╭┳╮╭┳━┳━━╮                                  "
+echo -e ${ylw}"* ┃╭━━┫╭┫╭╮┣┫┃━┫╭━┫┃╱╰━━╮┃╭╮┃╰╯┫┃┃┃╭┫╭╮┃                                  "
+echo -e ${ylw}"* ┃┃╱╱┃┃┃╰╯┃┃┃━┫╰━┫╰╮┃╰━╯┃╭╮┃╭╮┫╰╯┃┃┃╭╮┃                                  "
+echo -e ${ylw}"* ╰╯╱╱╰╯╰━━┫┣━━┻━━┻━╯╰━━━┻╯╰┻╯╰┻━━┻╯╰╯╰╯                                  "
+echo -e ${ylw}"* ╱╱╱╱╱╱╱╱╭╯┃                                                             "
+echo -e ${ylw}"* ╱╱╱╱╱╱╱╱╰━╯                                                             "
+echo -e ${cya}"* THANK YOU FOR BUILDING PROJECT SAKURA                                   "
+echo -e ${blu}"=========================================================================="
+echo -e ${cya}"You can use the below generated json to push OTA."
 echo " "
 echo "{"
 echo '"response": ['
@@ -18,11 +26,11 @@ echo ' "datetime":' "\"$(grep ro\.build\.date\.utc $OUT/system/build.prop | cut 
 echo ' "filename":' "\"$(basename $(ls $OUT/ProjectSakura*.zip))\","
 echo ' "id":' "\"$((cat $OUT/ProjectSakura*.zip.md5sum) | cut -d ' ' -f1)\","
 echo ' "romtype":'"\"nightly\","
-echo ' "size":' "\"$(stat -c%s $OUT/ProjectSakura*.zip)\","
+echo ' "size":' "$(stat -c%s $OUT/ProjectSakura*.zip)",
 echo ' "url":' "\"https://master.dl.sourceforge.net/project/projectsakura/$LINEAGE_BUILD/$(basename $(ls $OUT/ProjectSakura*.zip))\","
 echo ' "version":' "\"2.Q\""
 echo "}"
 echo "]"
 echo "}"
 echo " "
-echo "BUILD IS DONE!"
+echo -e ${grn}"BUILD IS DONE!"
