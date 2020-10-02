1b49adc250bc4139dc55bafc1e3bfd493637ed03
Pranav Vashi <neobuddy89@gmail.com>
2020-10-02 18:11:54 +0200

    Vendor: set some additional props
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -1,5 +1,6 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
+$(call inherit-product-if-exists, vendor/lineage/config/sakura.mk)
 
 # Bootanimation
 include vendor/lineage/config/bootanimation.mk
