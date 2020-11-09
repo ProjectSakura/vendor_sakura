1635ec27ceb8cc755d22437b2eb061f1d28f1ab9
LordShenron <shen.priyanshu@gmail.com>
2020-11-09 12:05:18 +0100

    Task: don't generate ota json at build completion
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>
    Signed-off-by: ReveRTX <rev9rt@gmail.com>

---
+++ build/tools/banner.sh
@@ -0,0 +1,21 @@
+# Specify colors utilized in the terminal
+red='\033[0;31m'             #  red
+grn='\033[0;32m'             #  green
+ylw='\033[0;33m'             #  yellow
+blu='\033[0;34m'             #  blue
+ppl='\033[0;35m'             #  purple
+cya='\033[0;36m'             #  cyan
+
+echo -e ${blu}"=========================================================================="
+echo -e ${ylw}"* ╭━━━╮╱╱╱╱╱╱╱╱╱╱╱╭╮╱╭━━━╮╱╱╭╮                                            "
+echo -e ${ylw}"* ┃╭━╮┃╱╱╱╱╭╮╱╱╱╱╭╯╰╮┃╭━╮┃╱╱┃┃                                            "
+echo -e ${ylw}"* ┃╰━╯┣━┳━━╋╋━━┳━┻╮╭╯┃╰━━┳━━┫┃╭┳╮╭┳━┳━━╮                                  "
+echo -e ${ylw}"* ┃╭━━┫╭┫╭╮┣┫┃━┫╭━┫┃╱╰━━╮┃╭╮┃╰╯┫┃┃┃╭┫╭╮┃                                  "
+echo -e ${ylw}"* ┃┃╱╱┃┃┃╰╯┃┃┃━┫╰━┫╰╮┃╰━╯┃╭╮┃╭╮┫╰╯┃┃┃╭╮┃                                  "
+echo -e ${ylw}"* ╰╯╱╱╰╯╰━━┫┣━━┻━━┻━╯╰━━━┻╯╰┻╯╰┻━━┻╯╰╯╰╯                                  "
+echo -e ${ylw}"* ╱╱╱╱╱╱╱╱╭╯┃    BY LORDSHENRON                                           "
+echo -e ${ylw}"* ╱╱╱╱╱╱╱╱╰━╯                                                             "
+echo -e ${cya}"* THANK YOU FOR BUILDING PROJECT SAKURA                                   "
+echo -e ${blu}"=========================================================================="
+echo -e ${cya}"You can execute the createjson.sh in vendor/lineage/build/tools to generate an OTA json for pushing OTA updates."
+
