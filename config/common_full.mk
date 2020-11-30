0dafabdfa4f40366766367ea673f17887840a0c2
LordShenron <shen.priyanshu@gmail.com>
2020-11-30 11:42:12 +0100

    Revert "Vendor: enable blur for all devices"
    This reverts commit 5c368712913af9a4df5db6d1e0a23e4dbebf38aa.

--- config/common_full.mk
+++ config/common_full.mk
@@ -16,8 +16,3 @@ PRODUCT_PACKAGES += \
 # Recorder
 PRODUCT_PACKAGES += \
     Recorder
-
-# Blur
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.sf.blurs_are_expensive=1 \
-    ro.surface_flinger.supports_background_blur=1
