10ecf397caa5b501312d3ae0c9446219c8243bca
Ashwin DS <astroashwin@outlook.com>
2021-10-10 14:27:55 +0000

    vendor: OTA json generator: switch to sha256 checksum for json
    Signed-off-by: Ashwin DS <astroashwin@outlook.com>

--- build/tools/createjson.sh
+++ build/tools/createjson.sh
@@ -12,7 +12,7 @@ echo '"response": ['
 echo "{"
 echo ' "datetime":' "\"$(grep ro\.build\.date\.utc $OUT/system/build.prop | cut -d= -f2)\","
 echo ' "filename":' "\"$(basename $(ls $OUT/ProjectSakura*.zip))\","
-echo ' "id":' "\"$((cat $OUT/ProjectSakura*.zip.md5sum) | cut -d ' ' -f1)\","
+echo ' "id":' "\"$((cat $OUT/ProjectSakura*.zip.sha256sum) | cut -d ' ' -f1)\","
 echo ' "romtype":'"\"nightly\","
 echo ' "size":' "$(stat -c%s $OUT/ProjectSakura*.zip)",
 echo ' "url":' "\"https://master.dl.sourceforge.net/project/projectsakura/$LINEAGE_BUILD/$(basename $(ls $OUT/ProjectSakura*.zip))\","
