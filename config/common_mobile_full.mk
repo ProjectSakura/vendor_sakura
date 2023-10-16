7fe1bbb23e056d91892e95cc732950eb0e87dcde
Pranav Vashi <neobuddy89@gmail.com>
2023-10-16 22:58:04 +0530

    sakura: Remove packages moved to addons
    Change-Id: I6af87bbb53245a407ca2566c0d708ef0da5f8b4c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common_mobile_full.mk
+++ config/common_mobile_full.mk
@@ -27,12 +27,6 @@ PRODUCT_PACKAGES += \
     unrar \
     zstd
 
-# Fonts
-PRODUCT_PACKAGES += \
-    fonts_customization.xml \
-    FontLatoOverlay \
-    FontRubikOverlay
-
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/dictionaries
