feb94bf1c24e278271a6f7a2d849443e66535b48
Pranav Vashi <neobuddy89@gmail.com>
2023-11-25 09:21:23 +0530

    common: Inherit pixel-fw when available
    Change-Id: I964ec68bd65c34414e2d303b08681ba23b39867d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: ReveRTX <rev9rt@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -1,7 +1,8 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
-$(call inherit-product-if-exists, vendor/addons/config.mk)
+$(call inherit-product-if-exists, vendor/addons/config.mk)o
 $(call inherit-product-if-exists, vendor/lineage/config/sakura.mk)
+$(call inherit-product-if-exists, vendor/pixel-framework/config.mk)
 
 # Bootanimation
 include vendor/lineage/config/bootanimation.mk
