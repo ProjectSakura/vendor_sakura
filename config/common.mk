cbae05388b6a143ebee753ee14f41ee0361c6b44
LordShenron <shen.priyanshu@gmail.com>
2024-03-24 14:14:29 +0100

    vendor: kill Lineage bootanimation
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -124,12 +124,6 @@ ifneq ($(TARGET_DISABLE_EPPE),true)
 $(call enforce-product-packages-exist-internal,$(lastword $(_include_stack)),product_manifest.xml rild Calendar android.hidl.memory@1.0-impl.vendor vndk_apex_snapshot_package)
 endif
 
-# Bootanimation
-TARGET_SCREEN_WIDTH ?= 1080
-TARGET_SCREEN_HEIGHT ?= 1920
-PRODUCT_PACKAGES += \
-    bootanimation.zip
-
 # Build Manifest
 PRODUCT_PACKAGES += \
     build-manifest
