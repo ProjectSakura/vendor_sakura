3d7757dee4e25450574f2ce3d58048d6dca25e33
LordShenron <shen.priyanshu@gmail.com>
2024-04-18 11:36:36 +0200

    vendor: don't include priv for officials
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -259,7 +259,7 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 
 # Official and Unoffical
 ifeq ($(SAKURA_OFFICIAL), true)
-include vendor/sakura-priv/keys.mk
+#include vendor/sakura-priv/keys.mk
     SAKURA_BUILD := OFFICIAL
     PRODUCT_PACKAGES += \
     Updater
