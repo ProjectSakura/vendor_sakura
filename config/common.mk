1947f1120a8dc0748537d4777c46382dcb3983b9
Pranav Vashi <neobuddy89@gmail.com>
2024-12-08 14:58:50 +0530

    sakura: Include vendor/certification when available
    Change-Id: Ia1148abcc7c333dc4ddc3f29ef2bdfdc78793724
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -2,6 +2,7 @@
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 $(call inherit-product-if-exists, vendor/lineage/config/sakura.mk)
 $(call inherit-product-if-exists, vendor/addons/config.mk)
+$(call inherit-product-if-exists, vendor/certification/config.mk)
 
 # Exclude kernel platform repos from bp scanning
 PRODUCT_SOURCE_ROOT_DIRS += -kernel/platform
