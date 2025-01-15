ae443aad7b1f8cb14ff1b4699beed6a94fb2980b
Joey Huab <jhuab@edukasyon.ph>
2025-01-15 11:37:37 +0900

    [TMP] config: Disable EPPE

--- config/common.mk
+++ config/common.mk
@@ -123,6 +123,8 @@ SYSTEMUI_OPTIMIZE_JAVA ?= true
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
+# Disable EPPE
+TARGET_DISABLE_EPPE ?= true
 ifneq ($(TARGET_DISABLE_EPPE),true)
 # Require all requested packages to exist
 $(call enforce-product-packages-exist-internal,$(wildcard device/*/$(LINEAGE_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml rild Calendar Launcher3 Launcher3Go Launcher3QuickStep Launcher3QuickStepGo android.hidl.memory@1.0-impl.vendor vndk_apex_snapshot_package)
