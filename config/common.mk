76f3585833d1c609446030cb581ab3108660af56
momenabdulrazek <momenabdulrazek@users.noreply.github.com>
2025-10-13 17:32:14 +0200

    Nuke offending build-manifest

--- config/common.mk
+++ config/common.mk
@@ -122,10 +122,6 @@ ifneq ($(TARGET_DISABLE_EPPE),true)
 $(call enforce-product-packages-exist-internal,$(lastword $(_include_stack)),product_manifest.xml rild Calendar android.hidl.memory@1.0-impl.vendor vndk_apex_snapshot_package)
 endif
 
-# Build Manifest
-PRODUCT_PACKAGES += \
-    build-manifest
-
 ifeq ($(PRODUCT_IS_AUTOMOTIVE),)
 PRODUCT_PACKAGES += \
     LineageParts \
