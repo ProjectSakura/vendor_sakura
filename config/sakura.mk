6781fc64d9af3c73579ce7d81661fe170eace575
momenabdulrazek <momenabdulrazek@users.noreply.github.com>
2025-11-30 22:38:08 +0100

    vendor_sakura: Build OmniJaws Service
    Change-Id: Ie19c0ab88d9a4f343493daf026b627b0591380b9

--- config/sakura.mk
+++ config/sakura.mk
@@ -38,3 +38,8 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 # Enable dex2oat64 to do dexopt
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     dalvik.vm.dex2oat64.enabled=true
+
+# Extra packages
+PRODUCT_PACKAGES += \
+    OmniJaws \
+    OmniStyle
