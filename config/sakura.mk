97806293bbf77eb6e79a3477adf7fe7e4443e68d
HeCheng Yu <kde-yyds@qq.com>
2025-11-30 22:53:31 +0100

    vendor_sakura: Disable default frame rate limit for games
    * Android 15 limits refresh rate to 60hz for games unless we enable
      the developer option 'Disable default frame rate for games' manually.
    * Adding this prop enables it by default.

--- config/sakura.mk
+++ config/sakura.mk
@@ -23,6 +23,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.input.video_enabled=false
 
+# Disable default frame rate limit for games
+PRODUCT_PRODUCT_PROPERTIES += \
+    debug.graphics.game_default_frame_rate.disabled=true
+
 # Enable blur
 TARGET_ENABLE_BLUR ?= false
 ifeq ($(TARGET_ENABLE_BLUR),true)
