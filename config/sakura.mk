888df0a6539813ac2bb3076e1ef269c9672eea05
ponces <ponces@users.noreply.github.com>
2025-11-30 22:43:40 +0100

    vendor_sakura: Switch to AOSPA's face unlock implementation

--- config/sakura.mk
+++ config/sakura.mk
@@ -47,3 +47,14 @@ PRODUCT_PACKAGES += \
     LMOFreeformSidebar \
     OmniJaws \
     OmniStyle
+
+ifneq ($(TARGET_FACE_UNLOCK_SUPPORTED),false)
+PRODUCT_PACKAGES += \
+    FaceUnlock
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/android.hardware.biometrics.face.xml
+endif
