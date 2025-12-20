779cc06da34cc1737e624792f2b766297413b2c6
momenabdulrazek <momenabdulrazek@users.noreply.github.com>
2025-12-20 20:22:34 +0100

    Build Missing updater package

--- config/common.mk
+++ config/common.mk
@@ -129,7 +129,8 @@ PRODUCT_PACKAGES += \
 endif
 
 PRODUCT_PACKAGES += \
-    LineageSettingsProvider
+    LineageSettingsProvider \
+    Updater
 
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.lineage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-updater.rc
