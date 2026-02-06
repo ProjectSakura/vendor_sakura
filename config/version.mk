68c6ced0ec7ed520d0b2bb95dc487fb35b8c1d4b
momenabdulrazek <momenabdulrazek@users.noreply.github.com>
2026-02-06 19:21:12 +0100

    Vendor: Sakura init       : Give option to user to select build type model
    Change-Id: Iff8bd8e3e393ebbefda820d3fa69f2be524bc4ad
    Co-authored-by: ReveRTX <rev9rt@gmail.com>
    Co-authored-by: momenabdulrazek <momen.abdelrazek33@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -1,45 +1,17 @@
-PRODUCT_VERSION_MAJOR = 23
+# Sakura Version
+PRODUCT_VERSION_MAJOR = 16
 PRODUCT_VERSION_MINOR = 2
+SAKURA_BUMP_VERSION = 0
 
-ifeq ($(LINEAGE_VERSION_APPEND_TIME_OF_DAY),true)
-    LINEAGE_BUILD_DATE := $(shell date -u +%Y%m%d_%H%M%S)
-else
-    LINEAGE_BUILD_DATE := $(shell date -u +%Y%m%d)
-endif
+# Increase Sakura Version with each major release.
+LINEAGE_DISPLAY_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR).$(SAKURA_BUMP_VERSION)
+LINEAGE_VERSION := ProjectSakura-$(LINEAGE_DISPLAY_VERSION)-$(shell date +%Y%m%d-%H%M)-$(SAKURA_BUILD_ZIP_TYPE)-$(LINEAGE_BUILD)-$(SAKURA_BUILD)
+SAKURA_VERSION := $(LINEAGE_VERSION)
 
-# Set LINEAGE_BUILDTYPE from the env RELEASE_TYPE, for jenkins compat
-
-ifndef LINEAGE_BUILDTYPE
-    ifdef RELEASE_TYPE
-        # Starting with "LINEAGE_" is optional
-        RELEASE_TYPE := $(shell echo $(RELEASE_TYPE) | sed -e 's|^LINEAGE_||g')
-        LINEAGE_BUILDTYPE := $(RELEASE_TYPE)
-    endif
-endif
-
-# Filter out random types, so it'll reset to UNOFFICIAL
-ifeq ($(filter RELEASE NIGHTLY SNAPSHOT EXPERIMENTAL,$(LINEAGE_BUILDTYPE)),)
-    LINEAGE_BUILDTYPE := UNOFFICIAL
-    LINEAGE_EXTRAVERSION :=
-endif
-
-ifeq ($(LINEAGE_BUILDTYPE), UNOFFICIAL)
-    ifneq ($(TARGET_UNOFFICIAL_BUILD_ID),)
-        LINEAGE_EXTRAVERSION := -$(TARGET_UNOFFICIAL_BUILD_ID)
-    endif
-endif
-
-LINEAGE_VERSION_SUFFIX := $(LINEAGE_BUILD_DATE)-$(LINEAGE_BUILDTYPE)$(LINEAGE_EXTRAVERSION)-$(LINEAGE_BUILD)
-
-# Internal version
-LINEAGE_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(LINEAGE_VERSION_SUFFIX)
-
-# Display version
-LINEAGE_DISPLAY_VERSION := $(PRODUCT_VERSION_MAJOR)-$(LINEAGE_VERSION_SUFFIX)
-
-# LineageOS version properties
+# SAKURA Versioning Props
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.lineage.version=$(LINEAGE_VERSION) \
     ro.lineage.display.version=$(LINEAGE_DISPLAY_VERSION) \
-    ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
-    ro.lineage.releasetype=$(LINEAGE_BUILDTYPE)
+    ro.sakura.ziptype=$(SAKURA_BUILD_ZIP_TYPE) \
+    ro.sakura.display.version=$(SAKURA_BUILD) \
+    ro.sakura.maintainer=$(SAKURA_MAINTAINER) \
+    ro.modversion=$(SAKURA_VERSION)
