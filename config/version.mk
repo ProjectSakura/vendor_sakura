03ca81dd9560da8b114a6dd16ca58853682aa0cd
momenabdulrazek <momenabdulrazek@users.noreply.github.com>
2026-04-08 21:29:46 +0200

    add pref_anim_override
    * fix for mtk and low-end devices perf regression caused by fixed dp translate and fade animation
    
    Co-authored-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Co-authored-by: Mo'men Abdulrazek Ali <momenabdulrazek@users.noreply.github.com>
    Change-Id: I397cc1c02418ab7dc458d6c2d9d2f61c4281c5d8

--- config/version.mk
+++ config/version.mk
@@ -16,6 +16,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.sakura.maintainer=$(SAKURA_MAINTAINER) \
     ro.modversion=$(SAKURA_VERSION)
 
+# flags
+PERF_ANIM_OVERRIDE ?= false
+
 # Signing
 ifeq ($(SAKURA_OFFICIAL),true)
   $(if $(wildcard vendor/sakura-priv/keys/keys.mk),,$(error Official builds require signing keys at vendor/sakura-priv/keys/keys.mk))
